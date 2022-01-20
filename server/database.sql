dd602647e27660867bbcaadf949ac20066b29b8e
Kacper Zawojski <zawojweb@gmail.com>
2022-01-20 23:36:26 +0100

    Create better DB

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,59 @@
 CREATE DATABASE perntodo;
 
-CREATE TABLE todo(
-    todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+CREATE TABLE Users(
+    user_id SERIAL PRIMARY KEY,
+    nickname VARCHAR(255) NOT NULL,
+    mail VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    avatar_id INT NOT NULL
+);
+
+CREATE TABLE Groups(
+   group_id SERIAL PRIMARY KEY,
+   name VARCHAR(255) NOT NULL,
+   privilege VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE User_Group(
+    user_id INT REFERENCES Users (user_id),
+    group_id INT REFERENCES Groups (group_id)
+);
+
+CREATE TABLE Messages(
+    message_id SERIAL PRIMARY KEY,
+    content VARCHAR(255) NOT NULL,
+    sendDate DATE NOT NULL
+);
+
+CREATE TABLE Messages_Group(
+    message_id INT REFERENCES Messages (message_id),
+    group_id INT REFERENCES Groups (group_id)
+);
+
+
+CREATE TABLE APIsKey(
+    apis_id SERIAL PRIMARY KEY,
+    NotionApiKey VARCHAR(2555),
+    DiscordApiKey VARCHAR(255),
+    FacebookApiKey VARCHAR(255),
+    SlackApiKey VARCHAR(255)
+);
+
+CREATE TABLE apis_groups(
+    apis_id INT REFERENCES APIsKey (apis_id),
+    group_id INT REFERENCES Groups (group_id)
+);
+
+
+CREATE TABLE Tasks (
+    task_id SERIAL PRIMARY KEY,
+    content VARCHAR(255) NOT NULL,
+    complete INT DEFAULT 0
+);
+
+
+CREATE TABLE Tasks_Group_User(
+    task_id INT REFERENCES Tasks (task_id),
+    group_id INT REFERENCES Groups (group_id),
+    user_id INT REFERENCES Users (user_id)
 );
--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg")
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "perntodo",
+  database: "inf0",
   password: "root",
   port: 5432,
 })
