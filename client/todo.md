124599157b259396475348f58d634920cb4a40c6
Kacper Zawojski <zawojweb@gmail.com>
2022-01-24 20:04:11 +0100

    Cleans and todoes

---
+++ client/todo.md
@@ -0,0 +1,3 @@
+# TODO:
+
+[] Add next.js functionality like getStaticProps. [] move Componetnts from pages to main root [] clean styles from className into Styled-compoentns with taiwindcss
--- server/index.js
+++ server/index.js
@@ -16,10 +16,3 @@ app.use("/group", require("./routes/groups"))
 app.listen(5000, () => {
   console.log(`Server listening on port 5000`)
 })
-
-//! TODO:
-// [] Add creating groups funcion, have to have: Normal query fo create groups, trigger for connect user with group and set privilage
-// [] Add creating APIs funcion, have to have: Normal query to add api, trigger for connect api with group
-// [] Add creating Tasks funcion, have to have: Normal query to add taks, trigger for connect tasks with group and user, trigger for new task
-// [] Message Just fithn how to implemet this stuff
-// [] Prepare routes like dashboard, for authorization which will say what view i have to load
